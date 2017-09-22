328358464ce2dc14f9f469da70534a9b2701c2f7
Alexander Zatserkovnyy <avz651@gmail.com>
2017-09-22 11:44:05 +1000

    conf file support for inotify-payload2wav is added

--- inotify-payload2wav.cpp
+++ inotify-payload2wav.cpp
@@ -2,7 +2,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 #include <unordered_map>
+#include <algorithm>
+
 #include <string.h>
 #include <errno.h>
 #include <poll.h>
@@ -10,6 +13,8 @@
 #include <stdlib.h>
 #include <sys/inotify.h>
 #include <unistd.h>
+#include <libpq-fe.h>
+
 
 extern "C"
 {
@@ -21,6 +26,61 @@ extern "C"
 using namespace std;
 
 const char* usage = "<path-to-payload-dir> <path-to-wav-dir>\n";
+
+typedef struct _wav_file_info {
+  string filename;
+  long double ts_opened;
+  long double ts_closed;
+  uint32_t samples; 
+} wav_file_info;
+
+bool cmp(wav_file_info a, wav_file_info b) { return (a.ts_opened<b.ts_opened); }
+
+#define PATH_TO_CONF "/data/conf/tap-rtpsave.conf"
+/* #define DB_COONNECTION "host=localhost dbname=voiplog user=dbworker password='vFcnbh_+'" */
+
+PGconn* conn;
+
+void exiterror(string mess)
+{
+  cerr << mess;
+  exit(1);
+}
+
+map<string,string> LoadConfig(string filename)
+{
+    ifstream in(filename);
+    map<string,string> res;
+    while(in)
+    {
+      string ln;
+      getline(in, ln);
+      string::size_type p_d=ln.find_first_of('=');
+      if( p_d != string::npos ){
+         string raw_key=ln.substr(0,p_d);
+         string::size_type pk_b = raw_key.find_first_not_of(" \t");
+         if( pk_b != string::npos ){
+            string::size_type pk_e = raw_key.find_last_not_of(" \t");
+            if( pk_e != string::npos ){
+               string raw_value =ln.substr(p_d+1,string::npos);
+               string::size_type pv_b = raw_value.find_first_not_of(" \t");
+               if( pv_b != string::npos ){
+                  string::size_type pv_e = raw_value.find_last_not_of(" \t");
+                  if( pv_e != string::npos ){
+                     res[raw_key.substr(pk_b,pk_e-pk_b+1)] = raw_value.substr(pv_b,pv_e-pv_b+1);
+                     string::size_type pos1 = raw_value.find_first_of("\"");
+                     string::size_type pos2 = raw_value.find_last_of("\"");
+                     if(pos1 != string::npos && pos2 != string::npos && pos2 > pos1) 
+                       res[raw_key.substr(pk_b,pk_e-pk_b+1)] = raw_value.substr(pos1+1,pos2-pos1-1);
+                  }
+               }
+            }
+         }
+      }
+    }
+    in.close();
+    return res;
+}
 
 int32_t decodeG729(vector<int16_t> & dest, const vector<unsigned char> & src)
 {
@@ -79,7 +139,7 @@ struct WAVHEADER
 } wavheader;
 #pragma pack(pop)
 
-int file2wav(string& filename, string& outputdir){
+int file2wav(string& payloaddir, string& filename, string& outputdir){
   int16_t numChannels=1; 
   int32_t sampleRate=8000;
   int16_t bitsPerSample=8;
@@ -104,7 +164,7 @@ int file2wav(string& filename, string& outputdir){
     return -1;
   }
   
-  ifstream pcm_input (filename, ifstream::binary | std::ifstream::in);
+  ifstream pcm_input (payloaddir+filename, ifstream::binary | std::ifstream::in);
   string output_name;
   pos=filename.find_last_of('/');
   if(pos==string::npos) output_name = filename; 
@@ -163,11 +223,19 @@ int file2wav(string& filename, string& outputdir){
      wav_output.write((char *)pRawData.data(),pRawData.size()*2);    
   else //g711a or g711mu, just save the payload to wav 
      wav_output.write(buffer,bytes_readed);
-
-  cout << output_name << " written: "<< wav_output.tellp() << endl;
+  
+  int numSamples = wavheader.subchunk2Size*8/(wavheader.numChannels*wavheader.bitsPerSample);
+  cout << output_name << " written: "<< wav_output.tellp() << " samples:" << numSamples << endl;
   wav_output.close();
-  delete [] buffer;
+  //SQL
+  PGresult* res;
+  string sqlrequest = "UPDATE files SET samples="+to_string(numSamples)+" WHERE filename='"+filename+"';";
+  res = PQexec(conn,sqlrequest.c_str());
+  if (PQresultStatus(res) != PGRES_COMMAND_OK) exiterror(PQresultErrorMessage(res));
+  PQclear(res);
 
+  //sql
+  delete [] buffer;
   return 0;
 }
 
@@ -189,22 +257,33 @@ static void handle_events(int fd, int wd, string &pl_path, string &wav_path ) {
     for (ptr = buf; ptr < buf + len; ptr += sizeof(struct inotify_event) + event->len) {
       event = (const struct inotify_event *) ptr;
       if (event->wd == wd ){
-	    string filename = pl_path + string(event->name);
-            file2wav(filename,wav_path);
+            string filename(event->name);
+            file2wav(pl_path,filename,wav_path);
       }
     }
   }
 }
 
 int main( int argc, char** argv ) {
+  string payloadpath;
+  string wavpath;
 
-  if (argc != 3) {  // We expect 3 arguments: the program name, a path to rtp payload directory, a path to wav files directory 
-      cerr << "Usage: " << argv[0] << usage;
-      return (EXIT_FAILURE);
-   }
+  if (argc > 1) payloadpath=string(argv[1]);
+  if (argc > 2) wavpath=string(argv[2]);
   
-  string payloadpath(argv[1]);
-  string wavpath(argv[2]);
+  map<string,string> conf=LoadConfig(string(PATH_TO_CONF));
+  auto it=conf.find("DB_COONNECTION");
+  if(it==conf.end()) exiterror("Can't find DB_COONNECTION configuration in the conf file "); 
+  string db_connection=it->second;
+  cout << "DB:" << db_connection << endl;
+
+  if(argc < 3){
+     it=conf.find("PATH_TO_STORAGE");
+     if(it==conf.end()) exiterror("Can't find PATH_TO_STORAGE configuration in the conf file ");
+     payloadpath= it->second + "/payload/";
+     wavpath= it->second + "/wav/";
+  } 
+
   if(payloadpath.at(payloadpath.length()-1)!='/') payloadpath +='/';
   if(wavpath.at(wavpath.length()-1)!='/') wavpath +='/';
 
@@ -218,7 +297,7 @@ int main( int argc, char** argv ) {
       return (EXIT_FAILURE);
   }
   
-  wd = inotify_add_watch(fd, argv[1], IN_CLOSE_WRITE);
+  wd = inotify_add_watch(fd, payloadpath.c_str(), IN_CLOSE_WRITE);
   if(wd == -1) {
       cerr << "Impossible to observe:" << payloadpath << endl;
       perror("inotify_add_watch");
@@ -228,6 +307,11 @@ int main( int argc, char** argv ) {
   nfds = 1;
   fds[0].fd = fd;
   fds[0].events = POLLIN;
+
+
+  conn = PQconnectdb(db_connection.c_str());
+
+  if (PQstatus(conn) != CONNECTION_OK) exiterror(PQerrorMessage(conn));
 
   cout << "Wait events."<< endl;
   while(1) {
@@ -243,6 +327,7 @@ int main( int argc, char** argv ) {
       }
   }
 
+  if (PQstatus(conn) == CONNECTION_OK) PQfinish(conn);
   return 0;
 }
 
